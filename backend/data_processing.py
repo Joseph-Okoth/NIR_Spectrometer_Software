8583b5ae0092d392d36d47d0f131a2a083f2a7d8
Joseph Onyach <josephokothonyach@gmail.com>
2025-03-19 17:58:40 +0300

    Synchronizing Backend

--- backend/data_processing.py
+++ backend/data_processing.py
@@ -4,3 +4,4 @@ from scipy.signal import find_peaks
 def process_data(wavelengths, intensities):
     peaks, _ = find_peaks(intensities, height=1000)
     return wavelengths[peaks]
+
--- backend/data_saving.py
+++ backend/data_saving.py
@@ -4,4 +4,5 @@ def save_to_csv(wavelengths, intensities, filename="spectrum_data.csv"):
     data = {'Wavelength': wavelengths, 'Intensity': intensities}
     df = pd.DataFrame(data)
     df.to_csv(filename, index=False)
+
     
